30c80a97250c775461b728dfa388c5e64d878de2
Your Name <you@example.com>
2022-07-13 08:27:31 +0000

    4d

---
+++ section_1/index_calculation_for_2d_grid2.cu
@@ -0,0 +1,50 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void index_calculation_for_2d_grid2(int *input)
+{
+    int gid = blockIdx.y * gridDim.x * blockDim.y * blockDim.x + blockIdx.x * blockDim.y * blockDim.x + threadIdx.y * blockDim.x + threadIdx.x;
+    printf("blockIdx.x : %d, blockIdx.y: %d, Idx : %d, value : %d\n", blockIdx.x, blockIdx.y, gid, input[gid]);
+}
+
+int main()
+{
+    int h_data[] = {
+        23,
+        9,
+        4,
+        53,
+        65,
+        12,
+        1,
+        33,
+        87,
+        45,
+        23,
+        12,
+        342,
+        56,
+        44,
+        99,
+    };
+
+    for (int i = 0; i < sizeof(h_data) / sizeof(int); i++)
+    {
+        printf("%d ", h_data[i]);
+    }
+    printf("\n \n");
+
+    int *d_data;
+    cudaMalloc((void **)&d_data, sizeof(h_data));
+    cudaMemcpy(d_data, h_data, sizeof(h_data), cudaMemcpyHostToDevice);
+
+    dim3 block(2, 2);
+    dim3 grid(2, 2);
+    index_calculation_for_2d_grid2<<<grid, block>>>(d_data);
+
+    cudaDeviceSynchronize();
+    cudaDeviceReset();
+    return 0;
+}
