05955bf879280996917b10e96df631c46c91da3c
Your Name <you@example.com>
2022-07-13 08:40:36 +0000

    add mem transfer

---
+++ section_1/memory_transfer.cu
@@ -0,0 +1,13 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <time.h>
+
+int main() {
+    
+    cudaDeviceReset();
+    return 0;
+}
