7d160b01feb9e42089548a78f9b66419445dfced
Your Name <you@example.com>
2022-08-16 11:59:06 +0000

    add sum array gpu

---
+++ section_1/sum_array_gpu.cu
@@ -0,0 +1,88 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstring>
+#include <time.h>
+#include <cassert>
+
+__global__ void sum_array_gpu(int *a, int *b, int *c, int size)
+{
+    int gid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (gid < size)
+    {
+        c[gid] = a[gid] + b[gid];
+    }
+}
+
+void sum_array_cpu(int *a, int *b, int *c, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        c[i] = a[i] + b[i];
+    }
+}
+
+void compare_arrays(int *h_c, int *gpu_results, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        assert(h_c[i] == gpu_results[i]);
+    }
+}
+
+int main()
+{
+    int size = 10000;
+    int block_size = 128;
+    int NO_BYTES = size * sizeof(int);
+
+    int *h_a = (int *)malloc(NO_BYTES);
+    int *h_b = (int *)malloc(NO_BYTES);
+    int *h_c = (int *)malloc(NO_BYTES);
+    int *gpu_results = (int *)malloc(NO_BYTES);
+
+    time_t t;
+    srand((unsigned)time(&t));
+    for (int i = 0; i < size; i++)
+    {
+        h_a[i] = (int)(rand() & 0xFF);
+    }
+    for (int i = 0; i < size; i++)
+    {
+        h_b[i] = (int)(rand() & 0xFF);
+    }
+
+    sum_array_cpu(h_a, h_b, h_c, size);
+    memset(gpu_results, 0, NO_BYTES);
+
+    int *d_a;
+    cudaMalloc((int **)&d_a, NO_BYTES);
+    int *d_b;
+    cudaMalloc((int **)&d_b, NO_BYTES);
+    int *d_c;
+    cudaMalloc((int **)&d_c, NO_BYTES);
+
+    cudaMemcpy(d_a, h_a, NO_BYTES, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, NO_BYTES, cudaMemcpyHostToDevice);
+
+    dim3 block(block_size);
+    dim3 grid((size / block.x) + 1);
+    sum_array_gpu<<<grid, block>>>(d_a, d_b, d_c, size);
+    cudaDeviceSynchronize();
+    cudaMemcpy(gpu_results, d_c, NO_BYTES, cudaMemcpyDeviceToHost);
+    compare_arrays(h_c, gpu_results, size);
+
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+
+    free(h_a);
+    free(h_b);
+    free(h_c);
+    free(gpu_results);
+
+    cudaDeviceReset();
+    return 0;
+}
